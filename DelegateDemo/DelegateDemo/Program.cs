ae305cf89831bb2399b86bac402377394a23c7bc
Pankaj Panjiar <pankaj.panjiar@icertis.com>
2022-04-22 18:39:12 +0530

    first version of delegate demo application

---
+++ DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        //Declare delegate
+        public delegate string ReportGenarator(int score);
+        static void Main(string[] args)
+        {
+            //Reference delegate to instance method
+            Report report = new Report();
+            ReportGenarator invoker = report.GenerateReport;
+            Console.WriteLine(invoker.Invoke(56));
+
+            //Reference delegate to static method
+            invoker = Report.GenerateReport_Static;
+            Console.WriteLine(invoker.Invoke(56));
+
+            //Using Func<> delegate type
+            Func<int, string> funcInvoker = Report.GenerateReport_Static;
+            Console.WriteLine(funcInvoker.Invoke(54));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DelegateDemo/DelegateDemo/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    public class Report
+    {
+        public string GenerateReport(int score)
+        {
+            return $"You have scored: {score}";
+        }
+
+        public static string GenerateReport_Static(int score)
+        {
+            return $"You have scored: {score}";
+        }
+
+        public void PrintReport(int score) => Console.WriteLine($"You have scored: {score}");
+    }
+}
